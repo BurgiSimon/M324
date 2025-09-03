a129c90ce8e984b4b05bb55462dcf54e4df65c64
BurgiSimon <simon.buergi@ggs.ch>
2025-09-03 11:49:46 +0200

    fix: new text PROJ-123

--- Aufgabe4/vue-project/src/App.vue
+++ Aufgabe4/vue-project/src/App.vue
@@ -1,11 +1,10 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>M324</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    Das hier ist ein Test
   </p>
-</template>
+</template> 
 
 <style scoped></style>
